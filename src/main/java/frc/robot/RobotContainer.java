66ab406aeaa42d64942af30f38c2f8a318caebe2
AndreasP64 <andpetrederp@gmail.com>
2025-01-07 18:40:12 -0500

    Added elevator subsystem. Added constants, robotcontainer base code.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,15 +6,41 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Elevator.ElevatorIOKraken;
+import frc.robot.subsystems.Elevator.ElevatorIOSim;
+import frc.robot.subsystems.Elevator.ElevatorIO;
+import frc.robot.subsystems.Elevator.Elevator;
+
 
 public class RobotContainer {
+
+  private Elevator m_elevatorSubsystem = null;
+
+
+  private final CommandXboxController driver = new CommandXboxController(0);
+  private final CommandXboxController operator = new CommandXboxController(1);
+
   public RobotContainer() {
+    switch (Constants.getRobot()) {
+      case COMPBOT -> {
+
+        m_elevatorSubsystem = new Elevator(new ElevatorIOKraken());
+        
+      }
+      case DEVBOT -> {}
+      case SIMBOT -> {
+        m_elevatorSubsystem = new Elevator(new ElevatorIOSim());
+      }
+    }
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+  /* Driver Controller */
+  private void configureBindings() {
 
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+
   }
+
 }
