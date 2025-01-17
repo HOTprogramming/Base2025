cd34a4e218585ca7b6d6bd7f5f68e9a0ac0e9bf7
AndreasP64 <andpetrederp@gmail.com>
2025-01-16 19:16:46 -0500

    manager copy paste

---
+++ src/main/java/frc/robot/subsystems/GameSpec/Manager.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems.GameSpec;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.GameSpec.Arm.Arm;
+import frc.robot.subsystems.GameSpec.Arm.ArmIOReal;
+import frc.robot.subsystems.GameSpec.Arm.ArmIOSim;
+import frc.robot.subsystems.GameSpec.Elevator.Elevator;
+import frc.robot.subsystems.GameSpec.Elevator.ElevatorIOReal;
+import frc.robot.subsystems.GameSpec.Elevator.ElevatorIOSim;
+
+public class Manager extends SubsystemBase{
+    private Arm armSubsystem;
+    private Elevator elevatorSubsystem;
+    
+    public Manager() {
+        switch (Constants.getRobot()) {
+          case COMPBOT -> {
+            elevatorSubsystem = new Elevator(new ElevatorIOReal());   
+            armSubsystem = new Arm(new ArmIOReal());
+          }
+          case DEVBOT -> {}
+          case SIMBOT -> {    
+            elevatorSubsystem = new Elevator(new ElevatorIOSim());
+            armSubsystem = new Arm(new ArmIOSim());
+    
+          }
+        }
+      }
+
+    // public void place(){
+    // elevatorSubsystem.managerElevatorTest();
+    // armSubsystem.managerArmTest();
+    // System.out.println("Made it to Manager");
+    // }
+
+    public Command place(){
+        return runOnce(() -> {elevatorSubsystem.managerElevatorTest();
+            armSubsystem.managerArmTest();});
+    }
+
+}
+
