80f314cb22da98452e97c35012054c49518e4828
AndreasP64 <andpetrederp@gmail.com>
2025-02-16 12:46:04 -0500

    changed constants system for gamespec to account for different comp bot, practice bot, and sim constants.

--- src/main/java/frc/robot/subsystems/GameSpec/Intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/GameSpec/Intake/IntakeConstants.java
@@ -1,21 +1,95 @@
 package frc.robot.subsystems.GameSpec.Intake;
 
+import frc.robot.Constants;
+
 public class IntakeConstants {
+
+    public static int intakeRollerID;
+    public static int intakeRotationID;
+    public static int intakeEncoderID;
+    public static double kReduction;
+    public static double kMaxAccelerationRpmPerSec;
+    public static MMGains intakeGains;
     
-    public static final int intakeRollerID = 18;
-    public static final int intakeRotationID = 19;
-    public static final int intakeEncoderID = 46;
-    public static final double kReduction = (1.0 / 2.0);
-    public static final double kMaxAccelerationRpmPerSec = 9000.0; 
-    public static final MMGains intakeGains = new MMGains(200, 100, 200, 20 , 2.5, 0.0, 0, 0);
-    
-    public static final double l4Height = 2;
-    public static final double l3Height = 1.5;
-    public static final double l2Height = 1;
-    public static final double l1Height = .5;
-    public static final double netHeight = 3;
-    public static final double intakeCoralHeight = 1.25;
-    public static final double intakeAlgaeHeight = .75;
+    public static double l4Height;
+    public static double l3Height;
+    public static double l2Height;
+    public static double l1Height;
+    public static double netHeight;
+    public static double intakeCoralHeight;
+    public static double intakeAlgaeHeight;
+
 
     public record MMGains(double CruiseVelocity, double Acceleration, double Jerk, double kP, double kI, double kD, double kV, double kS) {} 
+
+    static {
+        switch (Constants.getRobot()) {
+        case COMPBOT -> {
+          compBotConstants();
+        }
+        case DEVBOT -> {
+          practiceBotConstants();
+        }
+        case SIMBOT -> {    
+          simBotConstants();
+        }
+      }
+    }
+
+    private static void compBotConstants(){
+
+        intakeRollerID = 18;
+        intakeRotationID = 19;
+        intakeEncoderID = 46;
+        kReduction = (1.0 / 2.0);
+        kMaxAccelerationRpmPerSec = 9000.0; 
+        intakeGains = new MMGains(200, 100, 200, 20 , 2.5, 0.0, 0, 0);
+        
+        l4Height = 2;
+        l3Height = 1.5;
+        l2Height = 1;
+        l1Height = .5;
+        netHeight = 3;
+        intakeCoralHeight = 1.25;
+        intakeAlgaeHeight = .75;
+
+    }
+
+    private static void practiceBotConstants(){
+
+        intakeRollerID = 18;
+        intakeRotationID = 19;
+        intakeEncoderID = 46;
+        kReduction = (1.0 / 2.0);
+        kMaxAccelerationRpmPerSec = 9000.0; 
+        intakeGains = new MMGains(200, 100, 200, 20 , 2.5, 0.0, 0, 0);
+        
+        l4Height = 2;
+        l3Height = 1.5;
+        l2Height = 1;
+        l1Height = .5;
+        netHeight = 3;
+        intakeCoralHeight = 1.25;
+        intakeAlgaeHeight = .75;
+
+    }
+
+    private static void simBotConstants(){
+
+        intakeRollerID = 18;
+        intakeRotationID = 19;
+        intakeEncoderID = 46;
+        kReduction = (1.0 / 2.0);
+        kMaxAccelerationRpmPerSec = 9000.0; 
+        intakeGains = new MMGains(200, 100, 200, 20 , 2.5, 0.0, 0, 0);
+        
+        l4Height = 2;
+        l3Height = 1.5;
+        l2Height = 1;
+        l1Height = .5;
+        netHeight = 3;
+        intakeCoralHeight = 1.25;
+        intakeAlgaeHeight = .75;
+
+    }
 }
--- src/main/java/frc/robot/subsystems/GameSpec/Manipulator/ManipulatorConstants.java
+++ src/main/java/frc/robot/subsystems/GameSpec/Manipulator/ManipulatorConstants.java
@@ -1,29 +1,124 @@
 package frc.robot.subsystems.GameSpec.Manipulator;
+import frc.robot.Constants;
+import frc.robot.subsystems.GameSpec.Arm.Arm;
+import frc.robot.subsystems.GameSpec.Arm.ArmIOReal;
+import frc.robot.subsystems.GameSpec.Arm.ArmIOSim;
+import frc.robot.subsystems.GameSpec.Elevator.Elevator;
+import frc.robot.subsystems.GameSpec.Elevator.ElevatorIOReal;
+import frc.robot.subsystems.GameSpec.Elevator.ElevatorIOSim;
 
 public class ManipulatorConstants {
 
-    // CORAL AND ALGEA IDS ARE WRONG
-    public static final int coralMotorID = 16;//flipped
-    public static final int coralEncoderID = 54;
-    public static final int coralCandiID = 53;//sweetbox 6
-    public static final int coralWristID = 13;
-    public static final double kReduction = (1.0 / 2.0);
-    public static final double kMaxAccelerationRpmPerSec = 9000.0; 
-    public static final MMGains coralWristGains = new MMGains(3000, 4000, 14000, 0.2, 0.0, 0.002, 0.1, 0.0);
-    public static final VVGains coralSpinGains = new VVGains(10.0, 0.0, 0.0, 0, 1);
+      // CORAL AND ALGEA IDS ARE WRONG
+      public static int coralMotorID;
+      public static int coralEncoderID;
+      public static int coralCandiID;
+      public static int coralWristID;
+      public static double kReduction;
+      public static double kMaxAccelerationRpmPerSec;
+      public static MMGains coralWristGains;
+      public static VVGains coralSpinGains;
+  
+      public static int algaeArmID;
+      public static int algaeRollerID;
+      public static int algaeEncoderID;
+      public static MMGains algaeGains;
+      
+      public static double coralWristHP;
+      public static double coralWristScore;
+  
+      public static double algaeExtend;
+      public static double algaePackage;
 
-    public static final int algaeArmID = 17;
-    public static final int algaeRollerID = 25;
-    public static final int algaeEncoderID = 46;
-    public static final MMGains algaeGains = new MMGains(100, 100, 200, 1.0 , 0.0, 0.0, 0, 0);
+      public record MMGains(double CruiseVelocity, double Acceleration, double Jerk, double kP, double kI, double kD, double kV, double kS) {} 
+      public record VVGains(double kP, double kI, double kD, double kV, double kS) {}
     
-    public static final double coralWristHP = -90;
-    public static final double coralWristScore = 0;
 
-    public static final double algaeExtend = 0;
-    public static final double algaePackage = 0;
+    static {
+        switch (Constants.getRobot()) {
+        case COMPBOT -> {
+          compBotConstants();
+        }
+        case DEVBOT -> {
+          practiceBotConstants();
+        }
+        case SIMBOT -> {    
+          simBotConstants();
+        }
+      }
+    }
 
-    public record MMGains(double CruiseVelocity, double Acceleration, double Jerk, double kP, double kI, double kD, double kV, double kS) {} 
-    public record VVGains(double kP, double kI, double kD, double kV, double kS) {} 
+    private static void compBotConstants(){
+      
+      coralMotorID = 16;
+      coralEncoderID = 54;
+      coralCandiID = 53;
+      coralWristID = 13;
+      kReduction = (1.0 / 2.0);
+      kMaxAccelerationRpmPerSec = 9000.0; 
+      coralWristGains = new MMGains(3000, 4000, 14000, 0.2, 0.0, 0.002, 0.1, 0.0);
+      coralSpinGains = new VVGains(10.0, 0.0, 0.0, 0, 1);
+  
+      algaeArmID = 17;
+      algaeRollerID = 25;
+      algaeEncoderID = 46;
+      algaeGains = new MMGains(100, 100, 200, 1.0 , 0.0, 0.0, 0, 0);
+      
+      coralWristHP = -90;
+      coralWristScore = 0;
+  
+      algaeExtend = 0;
+      algaePackage = 0;
+
+
+    }
+
+    private static void practiceBotConstants(){
+
+      // CORAL AND ALGEA IDS ARE WRONG
+      coralMotorID = 16;//flipped
+      coralEncoderID = 54;
+      coralCandiID = 53;//sweetbox 6
+      coralWristID = 13;
+      kReduction = (1.0 / 2.0);
+      kMaxAccelerationRpmPerSec = 9000.0; 
+      coralWristGains = new MMGains(3000, 4000, 14000, 0.2, 0.0, 0.002, 0.1, 0.0);
+      coralSpinGains = new VVGains(10.0, 0.0, 0.0, 0, 1);
+  
+      algaeArmID = 17;
+      algaeRollerID = 25;
+      algaeEncoderID = 46;
+      algaeGains = new MMGains(100, 100, 200, 1.0 , 0.0, 0.0, 0, 0);
+      
+      coralWristHP = -90;
+      coralWristScore = 0;
+  
+      algaeExtend = 0;
+      algaePackage = 0;
+
+    }
+
+    private static void simBotConstants(){
+
+      coralMotorID = 16;
+      coralEncoderID = 54;
+      coralCandiID = 53;
+      coralWristID = 13;
+      kReduction = (1.0 / 2.0);
+      kMaxAccelerationRpmPerSec = 9000.0; 
+      coralWristGains = new MMGains(3000, 4000, 14000, 0.2, 0.0, 0.002, 0.1, 0.0);
+      coralSpinGains = new VVGains(10.0, 0.0, 0.0, 0, 1);
+  
+      algaeArmID = 17;
+      algaeRollerID = 25;
+      algaeEncoderID = 46;
+      algaeGains = new MMGains(100, 100, 200, 1.0 , 0.0, 0.0, 0, 0);
+      
+      coralWristHP = -90;
+      coralWristScore = 0;
+  
+      algaeExtend = 0;
+      algaePackage = 0;
+    }
 
 }
