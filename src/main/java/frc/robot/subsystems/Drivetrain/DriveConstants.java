e4bfdb398b6ed8880aa81fe1a5c5428204d3728f
Maya Vivio <101277689+09FALLDAMAGE@users.noreply.github.com>
2025-03-16 12:17:22 -0400

    cool 4 object (slow)

--- src/main/java/frc/robot/subsystems/Drivetrain/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/Drivetrain/DriveConstants.java
@@ -101,7 +101,7 @@ public class DriveConstants {
             put(Rotation2d.fromDegrees(60), new Double[] {7.0, -0.25,  -7.0});
             put(Rotation2d.fromDegrees(120), new Double[] {5.0, -0.25, -7.0});
             put(Rotation2d.fromDegrees(180), new Double[] {6.0, -0.25, -6.0});
-            put(Rotation2d.fromDegrees(-120), new Double[] {5.5, -0.25, -5.0});
+            put(Rotation2d.fromDegrees(-120), new Double[] {5.5, -0.25, -6.0}); // bad post on right (5")
             put(Rotation2d.fromDegrees(-60), new Double[] {5.5, -0.25, -6.5});
         }
     };
@@ -112,7 +112,7 @@ public class DriveConstants {
             put(Rotation2d.fromDegrees(60), new Double[] {6.0, -0.25,  -6.0});
             put(Rotation2d.fromDegrees(120), new Double[] {5.0, -0.25, -7.0});
             put(Rotation2d.fromDegrees(180), new Double[] {7.0, -0.25, -6.0});
-            put(Rotation2d.fromDegrees(-120), new Double[] {8.0, -0.25, -6.0});
+            put(Rotation2d.fromDegrees(-120), new Double[] {8.0, -0.25, -6.0}); 
             put(Rotation2d.fromDegrees(-60), new Double[] {6.0, -0.25, -6.0});
         }
     };
