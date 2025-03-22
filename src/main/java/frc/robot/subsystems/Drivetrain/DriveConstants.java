1a15182d39ae9c79d8570efcd28fb12f48769738
AndreasP64 <maya.j.vivio@gmail.com>
2025-03-22 12:32:47 -0400

    update -60

--- src/main/java/frc/robot/subsystems/Drivetrain/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/Drivetrain/DriveConstants.java
@@ -80,7 +80,7 @@ public class DriveConstants {
             put(Rotation2d.fromDegrees(120), new Double[] {5.5, -1.6, -6.0});
             put(Rotation2d.fromDegrees(180), new Double[] {6.0, -1.6, -6.0});
             put(Rotation2d.fromDegrees(-120), new Double[] {6.0, -1.6, -6.0});
-            put(Rotation2d.fromDegrees(-60), new Double[] {6.0, -1.6, -6.0});
+            put(Rotation2d.fromDegrees(-60), new Double[] {7.0, -1.6, -6.0});
         }
     };
 
